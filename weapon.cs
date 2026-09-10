5fcf6f740d3d667a5b7c0d675b5dc930934c4c51
CrosslithPCOD <1132810@hr.nl>
2026-09-10 16:54:42 +0200

    weapon.cs init

--- weapon.cs
+++ weapon.cs
@@ -5,18 +5,16 @@ public class Weapon
 	public int MaximumDamage;
     public int Health;
     public int Crit;
-    public int Amount;
     public bool Equippable;
     public bool Usable;
 
-	public Weapon(int id, string name, int damage, int health, int crit, int amount, bool equippable, bool usable)
+	public Weapon(int id, string name, int damage, int health, int crit, bool equippable, bool usable)
 	{
 		ID = id;
 		Name = name;
 		MaximumDamage = damage;
         Health = health;
         Crit = crit;
-        Amount = amount;
         Equippable = equippable;
         Usable = usable;
 	}
