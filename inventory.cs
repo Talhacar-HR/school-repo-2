6060440e64b89ded12e70e8d31d5352785322831
CrosslithPCOD <1132810@hr.nl>
2026-09-15 15:21:11 +0200

    Update inventory.cs

--- inventory.cs
+++ inventory.cs
@@ -6,32 +6,32 @@ class Inventory
 
     public void ViewInventory()
     {
-        ...
+        ...;
     }
 
     public void AddItem()
     {
-        ...
+        ...;
     }
 
     public void DropItem()
     {
-        ...
+        ...;
     }
 
     public void SwitchWeapon()
     {
-        ...
+        ...;
     }
 
     public void UseItem()
     {
-        ...
+        ...;
     }
 
     public bool HasInventorySpace()// checkt of er ruimte is (max 20)
     {
-        ...
+        ...;
     }
 
 
