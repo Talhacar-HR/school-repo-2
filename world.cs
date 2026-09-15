375b0ed7ad07dfa2f10bd74fe545aace2714c17a
CrosslithPCOD <1132810@hr.nl>
2026-09-15 16:15:46 +0200

    weapon types
    weapon types WEAPON, ARMOR and OTHER for distinction

--- world.cs
+++ world.cs
@@ -49,19 +49,19 @@ public static class World
 
     public static void PopulateWeapons()
     {
-        Weapons.Add(new Weapon(WEAPON_ID_RUSTY_SWORD, "Rusted Sword", 5, 0, 0, true, false, "An old, rusted sword. Raises damage by 10."));
-        Weapons.Add(new Weapon(WEAPON_ID_CLUB, "Club", 10, 0, 0, true, false, "A trusty wooden club. Raises damage by 10."));
-        Weapons.Add(new Weapon(WEAPON_ID_IRON_SWORD, "Iron Sword", 10, 0, 20, true, false, "An excellent iron sword. Raises damage by 10, and gives 20 CRIT."));
-        Weapons.Add(new Weapon(WEAPON_ID_COIN_POUCH, "Coin Pouch", 0, 0, 0, false, false, "A pouch with coins. Spend wisely."));
-        Weapons.Add(new Weapon(WEAPON_ID_PROOF_OF_GRIT, "Proof of Grit", 0, 0, 0, false, false, "A Proof of Grit obtained by completely quests. Needed to pass the guard."));
-        Weapons.Add(new Weapon(WEAPON_ID_FLASK_OF_HEALING, "Flask of Healing", 0, 15, 0, false, true, "A flask of healing. Heals 15 HP."));
-        Weapons.Add(new Weapon(WEAPON_ID_POTION_OF_HEALING, "Potion of Healing", 0, 25, 0, false, true, "A potion of healing. Heals 25 HP."));
-        Weapons.Add(new Weapon(WEAPON_ID_VIAL_OF_HEALING, "Vial of Healing", 0, 50, 0, false, true, "A vial of healing. Heals 50 HP."));
-        Weapons.Add(new Weapon(WEAPON_ID_FLASK_OF_STRENGTH, "Flask of Strength", 5, 0, 0, false, true, "A flask of strength. Raises attack by 5 for three attacks."));
-        Weapons.Add(new Weapon(WEAPON_ID_POTION_OF_STRENGTH, "Potion of Strength", 10, 0, 0, false, true, "A potion of strength. Raises attack by 10 for two attacks."));
-        Weapons.Add(new Weapon(WEAPON_ID_LEATHER_TUNIC, "Leather Tunic", 0, 10, 0, true, false, "A soft leather tunic. Raises Max HP by 10."));
-        Weapons.Add(new Weapon(WEAPON_ID_IRON_CHESTPLATE, "Iron Chestplate", 0, 30, 0, true, false, "A sturdy iron chestplate. Raises Max HP by 30."));
-        Weapons.Add(new Weapon(WEAPON_ID_MONSTER_ARMOR, "Monster Armor", 0, 20, 20, true, false, "A handcrafted set of monster armor. Raises Max HP by 20 and gives 20 CRIT."));
+        Weapons.Add(new Weapon(WEAPON_ID_RUSTY_SWORD, "Rusted Sword", 5, 0, 0, true, false, "WEAPON", "An old, rusted sword. Raises damage by 10."));
+        Weapons.Add(new Weapon(WEAPON_ID_CLUB, "Club", 10, 0, 0, true, false, "WEAPON", "A trusty wooden club. Raises damage by 10."));
+        Weapons.Add(new Weapon(WEAPON_ID_IRON_SWORD, "Iron Sword", 10, 0, 20, true, false, "WEAPON", "An excellent iron sword. Raises damage by 10, and gives 20 CRIT."));
+        Weapons.Add(new Weapon(WEAPON_ID_COIN_POUCH, "Coin Pouch", 0, 0, 0, false, false, "OTHER", "A pouch with coins. Spend wisely."));
+        Weapons.Add(new Weapon(WEAPON_ID_PROOF_OF_GRIT, "Proof of Grit", 0, 0, 0, false, false, "OTHER", "A Proof of Grit obtained by completely quests. Needed to pass the guard."));
+        Weapons.Add(new Weapon(WEAPON_ID_FLASK_OF_HEALING, "Flask of Healing", 0, 15, 0, false, true, "OTHER", "A flask of healing. Heals 15 HP."));
+        Weapons.Add(new Weapon(WEAPON_ID_POTION_OF_HEALING, "Potion of Healing", 0, 25, 0, false, true, "OTHER", "A potion of healing. Heals 25 HP."));
+        Weapons.Add(new Weapon(WEAPON_ID_VIAL_OF_HEALING, "Vial of Healing", 0, 50, 0, false, true, "OTHER", "A vial of healing. Heals 50 HP."));
+        Weapons.Add(new Weapon(WEAPON_ID_FLASK_OF_STRENGTH, "Flask of Strength", 5, 0, 0, false, true, "OTHER", "A flask of strength. Raises attack by 5 for three attacks."));
+        Weapons.Add(new Weapon(WEAPON_ID_POTION_OF_STRENGTH, "Potion of Strength", 10, 0, 0, false, true, "OTHER", "A potion of strength. Raises attack by 10 for two attacks."));
+        Weapons.Add(new Weapon(WEAPON_ID_LEATHER_TUNIC, "Leather Tunic", 0, 10, 0, true, false, "ARMOR", "A soft leather tunic. Raises Max HP by 10."));
+        Weapons.Add(new Weapon(WEAPON_ID_IRON_CHESTPLATE, "Iron Chestplate", 0, 30, 0, true, false, "ARMOR", "A sturdy iron chestplate. Raises Max HP by 30."));
+        Weapons.Add(new Weapon(WEAPON_ID_MONSTER_ARMOR, "Monster Armor", 0, 20, 20, true, false, "ARMOR", "A handcrafted set of monster armor. Raises Max HP by 20 and gives 20 CRIT."));
     }
 
     public static void PopulateMonsters()
