36bf14023ac28d294b81f55b3a0df2b368baea96
arian-skb <1085308@hr.nl>
2026-09-15 15:18:49 +0200

    inventory manage upd
    same changes as before, now saved

---
+++ inventory.cs
@@ -0,0 +1,107 @@
+using System.Net.ServerSentEvents;
+
+class Inventory
+{
+    public List<Weapon> inventory = [];
+
+    public void ViewInventory()
+    {
+        ...
+    }
+
+    public void AddItem()
+    {
+        ...
+    }
+
+    public void DropItem()
+    {
+        ...
+    }
+
+    public void SwitchWeapon()
+    {
+        ...
+    }
+
+    public void UseItem()
+    {
+        ...
+    }
+
+    public bool HasInventorySpace()// checkt of er ruimte is (max 20)
+    {
+        ...
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- weapon.cs
+++ weapon.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel;
-
 public class Weapon
 {
 	public int ID;
