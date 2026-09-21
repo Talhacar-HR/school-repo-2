3d448052aa5ebd3d5050fa1276e05b1c677219ee
Deniznasa <Denizyur@outlook.com>
2026-09-21 09:25:14 +0200

    Merge branch 'main' of https://github.com/Talhacar-HR/school-repo-2 into Battle-a-monster-2 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- inventory.cs
+++ inventory.cs
@@ -28,6 +28,7 @@ class Inventory
 
     public void DropItem(Weapon droppedWeapon)
     // What do we want to send from program.cs? The entire object selected in the drop item menu, or just the name?
+    // In world.cs there is a function called "WeaponByID", best to use that maybe?
     {
         bool found = false;
         foreach (Weapon item in inventory)
@@ -63,6 +64,7 @@ class Inventory
     public void UseItem(Weapon selectedWeapon)
     {
         // Cannot be implemented yet; battle logic needs to be added first
+        // This will cover potion usage during battle, aswell as coin pouch usage during shops later on
         break;
     }
 
--- location.cs
+++ location.cs
@@ -26,6 +26,66 @@ public class Location
         QuestAvailableHere = questavailablehere;
         MonsterLivingHere = monsterlivinghere;
     }
+
+
+     static void DisplayLocation(Location location)
+    {
+        Console.WriteLine();
+        Console.WriteLine("  P");
+        Console.WriteLine("  A");
+        Console.WriteLine("VFTGBS");
+        Console.WriteLine("  H");
+        Console.WriteLine("Your at " + location.Name);
+        Console.WriteLine(location.Description);
+    }
+
+    static Location Move(Location currentlocation)
+    {
+        Console.WriteLine("Which way do you want to move N,E,S,W");
+        string way = Console.ReadLine().ToUpper();
+
+        Location destination = GetDestination(currentLocation, way);
+    
+        if (destination == null)
+        {
+            Console.WriteLine("You can't go that way.");
+            return currentLocation;
+        }
+
+        return destination;
+    }
+
+
+    static Location GetDestination(Location currentlocation, string way)
+    {
+            if (way == "N")
+            {
+                return currentLocation.LocationToNorth;
+            }
+            else if (way == "S")
+            {
+                return currentLocation.LocationToSouth;
+            }
+            else if (way == "E")
+            {
+                return currentLocation.LocationToEast;
+            }
+            else if (way == "W")
+            {
+                return currentLocation.LocationToWest;
+            }
+            Console.WriteLine("You can't go that way.");
+            return null;
+            
+            
+    }
+
+
+
+
+
+
+
 }
 
 
