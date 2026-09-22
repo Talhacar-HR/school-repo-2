d03e012eec035f68611996407e42f8c46a9c5e2b
Deniznasa <Denizyur@outlook.com>
2026-09-22 14:11:38 +0200

    I added some methods to fight the monster and fiinished the class

--- program.cs
+++ program.cs
@@ -1,16 +1,39 @@
-using System;
+﻿using System;
 
 static class Program
 {
     static void Main()
     {
-        Location currentLocation = World.LocationByID(World.LOCATION_ID_HOME);
+        // Maak de speler aan en geef een startwapen
+        Player player = new Player("Hero", 50, 50);
+        player.CurrentWeapon = World.WeaponByID(World.WEAPON_ID_RUSTY_SWORD);
+        player.CurrentLocation = World.LocationByID(World.LOCATION_ID_HOME);
+
+        BattleMonster battleSystem = new BattleMonster();
 
         while (true)
         {
-            DisplayLocation(currentLocation);
-            currentLocation = Move(currentLocation);
+            Location previousLocation = player.CurrentLocation;
 
+            Location.DisplayLocation(player.CurrentLocation);
+            player.CurrentLocation = Location.Move(player.CurrentLocation);
+
+            // Controleer of er een monster op de nieuwe locatie is
+            if (player.CurrentLocation.MonsterLivingHere != null)
+            {
+                // Maak een kopie zodat de startstats gereset zijn
+                Monster baseMonster = player.CurrentLocation.MonsterLivingHere;
+                Monster battleMonster = new Monster(
+                    baseMonster.ID,
+                    baseMonster.Name,
+                    baseMonster.MaximumDamage,
+                    baseMonster.MaximumHitPoints,
+                    baseMonster.MaximumHitPoints,
+                    baseMonster.Boss
+                );
+
+                battleSystem.StartBattle(player, battleMonster, previousLocation);
+            }
         }
     }
 }
--- world.cs
+++ world.cs
@@ -72,7 +72,7 @@ public static class World
         Monster snake = new Monster(MONSTER_ID_SNAKE, "snake", 8, 20, 20);
 
 
-        Monster giantSpider = new Monster(MONSTER_ID_GIANT_SPIDER, "giant spider", 10, 40, 40);
+        Monster giantSpider = new Monster(MONSTER_ID_GIANT_SPIDER, "giant spider", 10, 40, 40, true);
 
         Monsters.Add(rat);
         Monsters.Add(snake);
@@ -132,7 +132,7 @@ public static class World
         Location bridge = new Location(LOCATION_ID_BRIDGE, "Bridge", "A stone bridge crosses a wide river.", null, null);
         bridge.QuestAvailableHere = QuestByID(QUEST_ID_COLLECT_SPIDER_SILK);
 
-        Location spiderField = new Location(MONSTER_ID_GIANT_SPIDER, "Forest", "You see spider webs covering covering the trees in this forest.", null, null);
+        Location spiderField = new Location(LOCATION_ID_SPIDER_FIELD, "Forest", "You see spider webs covering covering the trees in this forest.", null, null);
         spiderField.MonsterLivingHere = MonsterByID(MONSTER_ID_GIANT_SPIDER);
 
         // Link the locations together
